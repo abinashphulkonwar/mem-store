0574bd76c15045b9ab2c75760fbdf4517abac6cb
abinash <abinashphulkonwar1@gmail.com>
2023-09-18 10:13:11 +0530

    add get handler and List

--- stroage/Hash-table.h
+++ stroage/Hash-table.h
@@ -2,6 +2,9 @@
 #define HASHTABLE_H
 #include <unordered_map>
 #include <iostream>
+#include <variant>
+#include "../status-code/status-code.h"
+#include "List.h"
 
 using namespace std;
 
@@ -9,6 +12,7 @@ struct Hash_Table_Records
 {
     string text;
     int number;
+    LinkedList *List;
     int type; // 0 to text, 1 to number, 2 to list
 };
 
@@ -20,9 +24,11 @@ private:
 public:
     HTable(/* args */);
     void SetText(string Key, string value);
+    pair<int, variant<int, string>> GetHandler(string key);
     tuple<bool, int, string> SetNumber(string Key, string value);
     pair<bool, int> GetNumber(string Key);
     string GetText(string Key);
+    void CleareKey(string key);
     ~HTable();
 };
 
